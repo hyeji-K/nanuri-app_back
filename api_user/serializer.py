b7e4c975a87ec1b84cfa962fc675dfb4e494c4cf
Khyeji <khjji0502@gmail.com>
2021-12-01 22:19:54 +0900

    feat: 1201 add category model

--- api_user/serializer.py
+++ api_user/serializer.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
-from .models import User, Product
+from .models import Product, Category, User, SocialLogin
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialLogin
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
--- api_user/urls.py
+++ api_user/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name = 'api_user'
 
 urlpatterns = [
+    path('logins/', views.LoginView.as_view()),
+    path('logins/<int:id>', views.LoginView.as_view()),
     path('users/', views.UserView.as_view()),
-    path('users/<int:uid>', views.UserView.as_view()),
-    path('users/<int:uid>/regiproducts/', views.ProductView.as_view()),
+    path('users/<int:user_id>', views.UserView.as_view()),
+    path('category/', views.CategoryView.as_view()),
+    path('category/<int:category_id>', views.CategoryView.as_view()),
+    # path('users/<int:uid>/regiproducts/', views.ProductView.as_view()),
     path('products/', views.ProductView.as_view()),
-    # path('products/<int:uid>', views.ProductView.as_view()),
+    path('products/<int:uid>', views.ProductView.as_view()),
 ]
