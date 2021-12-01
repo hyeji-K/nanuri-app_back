a17f99e599a2d14a6e0f28ed6e343b2e0b098466
Khyeji <khjji0502@gmail.com>
2021-12-01 22:19:54 +0900

    feat: 1201 add category model

--- api_user/models.py
+++ api_user/models.py
@@ -2,45 +2,65 @@ from django.db import models
 from django.db.models.expressions import F
 import uuid
 
+from django.db.models.fields import AutoField
+
 # Create your models here.
+class SocialLogin(models.Model):
+    id = models.AutoField(primary_key=True, null=False, unique=True)
+    social_id = models.CharField(max_length=128, null=False, unique=True)
+
+    class Meta:
+        db_table = "SocialLogin"
+
 class User(models.Model):
     # userid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # db의 id랑 겹치는데 왜 때문인지??? 
-    id = models.AutoField(primary_key=True)
-    userEmail = models.EmailField(max_length=100, unique=True, null=False) # 사용자의 이메일 
-    userArea = models.CharField(max_length=100, null=False) # 사용자의 동네 
-    userName = models.CharField(max_length=50, unique=True, null=False) # 사용자의 닉네임
-    ratingScore = models.IntegerField(default=0, null=True) # 사용자의 점수 
-    createdAt = models.DateTimeField(auto_now_add=True, null=False) # 사용자의 가입일
-    # activated = models.BooleanField(null=True) # 사용자의 활성화 상태 (1 또는 2)
-    userBank = models.CharField(max_length=50, null=True) # 사용자의 은행 이름 
-    bankNum = models.IntegerField(null=True) # 사용자의 은행 계좌
-    userNumber = models.IntegerField(null=True) # 사용자의 전화번호
+    user_id = models.AutoField(primary_key=True, null=False, unique=True)
+    # user_email = models.EmailField(max_length=128, unique=True, null=False) # 사용자의 이메일 
+    user_area = models.CharField(max_length=128, null=False) # 사용자의 동네 
+    user_nick = models.CharField(max_length=128, unique=True, null=False) # 사용자의 닉네임
+    score = models.IntegerField(default=0, null=True) # 사용자의 점수 
+    user_bank = models.CharField(max_length=32, blank=True, null=True) # 사용자의 은행 이름 
+    banknum = models.IntegerField(blank=True, null=True) # 사용자의 은행 계좌
+    user_number = models.IntegerField(blank=True, null=True) # 사용자의 전화번호
+    created_date = models.DateTimeField(auto_now_add=True, null=False) # 사용자의 가입일
+    modify_date = models.DateTimeField(auto_now=True) # 수정 날짜
+    # activated = models.BooleanField(null=True) # 사용자의 활성화 상태 (0 또는 1) 로그인하면 1 탈퇴하면 0
 
+    social_id = models.ForeignKey(SocialLogin, null=False, on_delete=models.CASCADE, db_column='social_id')
+    
     class Meta:
         db_table = "User"
 
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True, null=False)
+    category_name = models.CharField(max_length=50, null=False)
+
+    class Meta:
+        db_table = "Categoey"
+
 class Product(models.Model):
     # no = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
-    # category = models.CharField
-    productName = models.CharField(max_length=100, null=False)
-    # link = models.URLField()
+    product_id = AutoField(primary_key=True, null=False, unique=True)
+    product_name = models.CharField(max_length=100, null=False)
+    link = models.URLField(max_length=200, null=True)
     # productImage = models.ImageField(up) # 이미지는 나중에 허허 .. 
-    productPrice = models.IntegerField(null=False)
-    totalPplCnt = models.IntegerField(null=False)
-    joinPplCnt = models.IntegerField(null=True)
-    startPeriod = models.DateField(auto_now_add=True, null=False)
-    endPeriod = models.DateField(null=False)
-    deliveryMethod = models.CharField(max_length=10, null=False)
-    detailContent = models.CharField(max_length=100, null=True)
-    createdAt = models.DateTimeField(auto_now_add=True, null=False)
+    product_price = models.IntegerField(null=False)
+    total_ppl_cnt = models.IntegerField(null=True)
+    join_ppl_cnt = models.IntegerField(null=True, default=1)
+    start_date = models.DateField(auto_now_add=True, null=False)
+    end_date = models.DateField(null=False)
+    delivery_method = models.CharField(max_length=10, null=False)
+    detail_content = models.CharField(max_length=200, null=True)
+    create_at = models.DateTimeField(auto_now_add=True, null=False)
+    modify_date = models.DateTimeField(auto_now=True)
     # activated = models.BooleanField # 활성화 상태
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    category_id = models.ForeignKey(Category, db_column='category_id', on_delete=models.CASCADE)
 
     class Meta:
         db_table = "Product"
 
-# class Categorys(models.Model):
-#     categoryName = models.CharField(max_length=50, null=False)
 
 
 # class BuyProductList(models.Model):
--- api_user/views.py
+++ api_user/views.py
@@ -1,55 +1,107 @@
+from django.http import request
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api_user.models import User, Product
-from api_user.serializer import UserSerializer, ProductSerializer
-from rest_framework import status
+from api_user.models import Product, Category, User, SocialLogin
+from api_user.serializer import ProductSerializer, CategorySerializer, UserSerializer, LoginSerializer
+from rest_framework import serializers, status
 
 # Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        login_serializer = LoginSerializer(data=request.data)
+        if login_serializer.is_valid():
+            login_serializer.save()
+            return Response({'create':'success', 'data':login_serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'create':'fail', 'error':login_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        login_queryset = SocialLogin.objects.all()
+        login_serializer = LoginSerializer(login_queryset, many=True)
+        return Response({'count':login_queryset.count(), 'data':login_serializer.data}, status=status.HTTP_200_OK)
+    
+    # 나중에 수정
+    def delete(self, request, **kwargs):
+        if kwargs.get('id') is None:
+            return Response({'delete':'fail', 'error':"Invalid request".data}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            id = kwargs.get('id')
+            login_object = SocialLogin.objects.get(id=id)
+            login_object.delete()
+            return Response({'delete':'success', 'message':"delete ok"}, status=status.HTTP_200_OK)
+
 
 class UserView(APIView):
     def post(self, request):
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response({'success':'true', 'data':user_serializer.data}, status=status.HTTP_200_OK)
+            return Response({'create':'success', 'data':user_serializer.data}, status=status.HTTP_200_OK)
         else:
-            return Response({'success':'false', 'error':user_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'create':'fail', 'error':user_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, **kwargs):
-        if kwargs.get('uid') is None:
+        if kwargs.get('user_id') is None:
             user_queryset = User.objects.all()
             user_serializer = UserSerializer(user_queryset, many=True)
             return Response({'count':user_queryset.count(), 'data':user_serializer.data}, status=status.HTTP_200_OK)
         else:
-            sno = kwargs.get('uid')
-            user_queryset = UserSerializer(User.objects.get(id=sno))
+            id = kwargs.get('user_id')
+            user_queryset = UserSerializer(User.objects.get(user_id=id))
+
+            # return Response({'user':user_queryset.data}, status=status.HTTP_200_OK)
+
             # 등록한 상품
-            products = Product.objects.filter(user_id=sno)
+            products = Product.objects.filter(user_id=id)
             product_serializer = ProductSerializer(products, many=True)
             return Response({'user':user_queryset.data, 'products':product_serializer.data}, status=status.HTTP_200_OK)
 
     def put(self, request, **kwargs):
-        if kwargs.get('uid') is None:
-            return Response({'success':'false', 'error':"Invalid request"}, status=status.HTTP_404_NOT_FOUND)
+        if kwargs.get('user_id') is None:
+            return Response({'result':'fail', 'error':"Invalid request"}, status=status.HTTP_404_NOT_FOUND)
         else:
-            uid = kwargs.get('uid')
-            user_object = User.objects.get(id=uid)
+            id = kwargs.get('user_id')
+            user_object = User.objects.get(user_id=id)
             update_serializer = UserSerializer(user_object, data=request.data)
             if update_serializer.is_valid():
                 update_serializer.save()
-                return Response({'update':'true', 'data':update_serializer.data}, status=status.HTTP_200_OK)
+                return Response({'update':'success', 'data':update_serializer.data}, status=status.HTTP_200_OK)
             else:
-                return Response({'update':'false', 'error':"Invalid request".data}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'update':'fail', 'error':"Invalid request".data}, status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, **kwargs):
-        if kwargs.get('uid') is None:
-            return Response({'delete':'false', 'error':"Invalid request".data}, status=status.HTTP_404_NOT_FOUND)
+        if kwargs.get('user_id') is None:
+            return Response({'delete':'fail', 'error':"Invalid request".data}, status=status.HTTP_404_NOT_FOUND)
         else:
-            uid = kwargs.get('uid')
-            user_object = User.objects.get(id=uid)
+            id = kwargs.get('user_id')
+            user_object = User.objects.get(user_id=id)
             user_object.delete()
-            return Response({'delete':'true', 'message':"delete ok"}, status=status.HTTP_200_OK)
+            return Response({'delete':'success', 'message':"delete ok"}, status=status.HTTP_200_OK)
+
+# Category
+class CategoryView(APIView):
+    # 카테고리 추가 완
+    # def post(self, request):
+    #     category_serializer = CategorySerializer(data=request.data)
+    #     if category_serializer.is_valid():
+    #         category_serializer.save()
+    #         return Response(category_serializer.data, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response({'success':'false', 'error':category_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, **kwargs):
+        if kwargs.get('category_id') is None:
+            category_queryset = Category.objects.all()
+            category_serializer = CategorySerializer(category_queryset, many=True)
+            return Response({'count':category_queryset.count(), 'categorys':category_serializer.data}, status=status.HTTP_200_OK)
+        else:
+            id = kwargs.get('category_id')
+            category_serializer = CategorySerializer(Category.objects.get(category_id=id))
+            # 등록한 상품
+            products = Product.objects.filter(category_id=id)
+            product_serializer = ProductSerializer(products, many=True)
+            return Response({'category':category_serializer.data, 'products':product_serializer.data}, status=status.HTTP_200_OK)
 
 
 # Product
@@ -58,14 +110,14 @@ class ProductView(APIView):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'success':'true', 'data':serializer.data}, status=status.HTTP_200_OK)
+            return Response({'success':'true', 'products':serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({'success':'false', 'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, **kwargs):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
-        return Response({'count':queryset.count(), 'data':serializer.data}, status=status.HTTP_200_OK)
+        return Response({'count':queryset.count(), 'products':serializer.data}, status=status.HTTP_200_OK)
 
     def put(self, request, **kwargs):
         if kwargs.get('uid') is None:
